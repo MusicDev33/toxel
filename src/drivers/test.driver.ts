970622081de718e7dbdd37b34d79a40e8237c517
MusicDev33 <smccowan33@gmail.com>
2020-12-30 12:53:41 -0700

    Finished test suite

--- src/drivers/test.driver.ts
+++ src/drivers/test.driver.ts
@@ -1,17 +1,18 @@
-import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
+import { execSync } from 'child_process';
 import { createOneBackup } from '@drivers/backup.driver';
 
 const testFolder = path.join(__dirname, '../../test');
 const root = path.join(__dirname, '../../');
+const outputFolder = path.join(root, 'testarchive');
+const testArchiveName = 'testarchive.tar.gz';
+const testText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed\n';
 
 export const createTestFiles = () => {
-  const testText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed\n';
-
-  if (!fs.existsSync(testFolder)){
+  if (!fs.existsSync(testFolder)) {
     fs.mkdirSync(testFolder);
-}
+  }
 
   for (let i = 0; i < 10; i++) {
     const filePath = path.join(testFolder, `test${i}.txt`);
@@ -20,9 +21,30 @@ export const createTestFiles = () => {
 }
 
 export const archiveTestFiles = () => {
-  createOneBackup(path.join(root, 'test.tar.gz'), testFolder, {sync: true});
+  createOneBackup(path.join(root, testArchiveName), testFolder, {sync: true});
+}
+
+export const checkArchiveTestFiles = () => {
+  const archive = path.join(root, testArchiveName);
+  fs.mkdirSync(outputFolder);
+  const stdout = execSync(`tar -xvzf ${archive} -C ${outputFolder}`);
+
+  const unzippedTestFolder = path.join(outputFolder, 'test');
+  fs.readdirSync(unzippedTestFolder).forEach((file) => {
+    const contents = fs.readFileSync(path.join(unzippedTestFolder, file)).toString('utf8');
+    if (contents != testText) {
+      console.log(`Test for ${file} - failed, aborting now.`);
+      return;
+    }
+
+    console.log(`Test for ${file} - okay`);
+  });
+
+  console.log('Test suite success. Cleaning up now...');
 }
 
 export const cleanUpTesting = () => {
   fs.rmdirSync(testFolder, { recursive: true});
+  fs.rmdirSync(outputFolder, { recursive: true});
+  fs.unlinkSync(path.join(root, testArchiveName));
 }
--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 // tslint:disable-next-line
 require('tsconfig-paths/register');
-import { createTestFiles, archiveTestFiles, cleanUpTesting } from '@drivers/test.driver';
+import { createTestFiles, archiveTestFiles, checkArchiveTestFiles, cleanUpTesting } from '@drivers/test.driver';
 
 createTestFiles();
 archiveTestFiles();
+checkArchiveTestFiles();
 cleanUpTesting();
