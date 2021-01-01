a57ccac17aaf8a3612c762af10207ca65a18e029
MusicDev33 <smccowan33@gmail.com>
2021-01-01 15:43:24 -0700

    Update packages, start manager driver

---
+++ src/drivers/manager.driver.ts
@@ -0,0 +1,15 @@
+/*
+  This is the manager driver. Part of our requirements are that we
+  share storage with another department, so we must make sure we don't go over our share.
+  If we exceed our share of storage on the NAS, then the other department's Windows servers
+  shut down. The manager manages our part of the storage, and makes sure usage doesn't
+  get too high. The point is to make sure storage isn't ever exceeded during a time
+  when no one is available to fix the issue.
+*/
+
+
+// TODO: Delete oldest backup
+// TODO: Detect storage space left, act accordingly
+export const manageDriver = () => {
+
+}
